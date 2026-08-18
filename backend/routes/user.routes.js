af342ae8f07a5f84a752a14d1873da572bd34eeb
hellkryptonium <harishjs1006@gmail.com>
2026-08-19 00:39:41 +0530

    Implement user profile update and retrieval endpoints

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, register, uploadProfilePicture } from "../controllers/user.controller.js";
+import { getUserAndProfile, login, register, updateUserProfile, uploadProfilePicture } from "../controllers/user.controller.js";
 import multer from "multer";
 
 const router = Router();
@@ -20,5 +20,7 @@ router.route("/upload_profiel_picture")
 
 router.route("/register").post(register);
 router.route("/login").post(login)
+router.route("/user_update").post(updateUserProfile)
+router.route(("/get_user_and_profile")).get(getUserAndProfile);
 
 export default router;
